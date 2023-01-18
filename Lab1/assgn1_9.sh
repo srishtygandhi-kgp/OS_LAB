b711510c3f66276a0b119fc84e1193e3547b9ae9
Yatindra Indoria <84220034+yatindra7@users.noreply.github.com>
2023-01-18 22:27:25 +0530

    17 words, gives incorrect output on my system

--- Lab1/assgn1_9.sh
+++ Lab1/assgn1_9.sh
@@ -1,4 +1,6 @@
-awk -F ' ' '{major[$2]++} END{for (i in major) print i, major[i]}' "$1" | sort -nk1 | sort -rsnk2
+#awk -F ' ' '{major[$2]++} END{for (i in major) print i, major[i]}' "$1" | sort -nk1 | sort -rsnk2
+#echo
+#awk -F ' ' '{student[$1]++} END{for (i in student) {if (student[i]>1) {print i;} else {cnt++}} print cnt; }' "$1"
+awk '{major[$2]++}END{for(i in major){print i, major[i]}}' "$1"|sort -nk1|sort -rsnk2
 echo
-awk -F ' ' '{student[$1]++} END{for (i in student) {if (student[i]>1) {print i;} else {cnt++}} print cnt; }' "$1"
-
+awk '{student[$1]++}END{for(i in student){if(student[i]>1){print i;}else{cnt++}}print cnt;}' "$1"
