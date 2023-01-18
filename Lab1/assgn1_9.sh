286745be6c2da98bc2b82983aaea74399b950f25
Sarita-Singh <sarita01.ar@gmail.com>
2023-01-18 15:28:45 +0530

    39 words

---
+++ Lab1/assgn1_9.sh
@@ -0,0 +1,4 @@
+awk -F ' ' '{major[$2]++} END{for (i in major) print i, major[i]}' "$1" | sort -nk1 | sort -rsnk2
+echo
+awk -F ' ' '{student[$1]++} END{for (i in student) {if (student[i]>1) {print i;} else {cnt++}} print cnt; }' "$1"
+
