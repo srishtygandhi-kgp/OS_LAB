5d990faa96cd21edaf7bc888c0a4d147a9236b69
Sarita-Singh <sarita01.ar@gmail.com>
2023-01-22 17:34:22 +0530

    changed acc to updated ques

--- Lab1/assgn1_6.sh
+++ Lab1/assgn1_6.sh
@@ -1,8 +1,8 @@
-sieve="$(seq 2 1000000|sort)"
-for n in 2 $(seq 3 2 1000000)
+sieve="$(seq 2 1000|sort)"
+for n in 2 $(seq 3 2 1000)
 do
-  sieve="$(comm -23 <(echo "$sieve") <(seq $(($n * $n)) $n 1000000|sort))"
+  sieve="$(comm -23 <(echo "$sieve") <(seq $(($n * $n)) $n 1000|sort))"
 done
 while read line; do
-  echo "$sieve" | sort -n | awk -v inputlimit="$line" '$1 <= inputlimit { print }'   | sed ':a;N;$!ba;s/\n/\ /g'
+  echo "$sieve" | sort -n | awk -v inputlimit="$line" '$1 <= inputlimit { if(inputlimit%$1==0) print }'   | sed ':a;N;$!ba;s/\n/\ /g'
 done < "$1" > "output.txt"
