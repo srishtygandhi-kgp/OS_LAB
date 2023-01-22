72d31a0b06cbdb34663d8347fa21861e8ceec51f
Sarita-Singh <sarita01.ar@gmail.com>
2023-01-22 11:44:27 +0530

    35 words

--- Lab1/assgn1_7.sh
+++ Lab1/assgn1_7.sh
@@ -1,14 +1,9 @@
-[ -d $2 ] || mkdir -p $2
+[ -d $2 ]||mkdir -p $2
 declare -A names
-for file in $1/*.txt; do
-    while read -a line; do
-        names[$(echo "${line:0:1}" | awk '{print tolower($1)}')]="${names[$(echo "${line:0:1}" | awk '{print tolower($1)}')]} $line";
-    done < $file
+for file in $1/*.txt;do
+    while read -a line;do
+        key=$(echo "${line:0:1}"|awk '{print tolower($1)}')
+        names[$key]="${names[$key]} $line";
+    done<$file
 done
-for x in {a..z}
-do  
-    echo "${names[$x]}" | tr " " "\n" | sort -n > $2/${x}.txt
-done
- 
-
-
+for x in {a..z};do echo "${names[$x]}"|tr " " "\n"|sort -n>$2/${x}.txt;done
