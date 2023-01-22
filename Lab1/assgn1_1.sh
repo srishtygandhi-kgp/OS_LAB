6084efec8172f3d80d45575ed7cce85a51c09761
srishtygandhi-kgp <101906706+srishtygandhi-kgp@users.noreply.github.com>
2023-01-22 23:27:12 +0530

    54 words, change to handle int overflow

--- Lab1/assgn1_1.sh
+++ Lab1/assgn1_1.sh
@@ -1,1 +1,7 @@
-lcm=1;while read x;do reverse=$(echo $x|rev);if((reverse!=0));then a=$lcm;b=$reverse;while((b!=0));do r=$((a%b));a=$b;b=$r;done;lcm=$((lcm*reverse/a));fi;done<$1;echo $lcm
+lcm=1&&while IFS= read -r line
+do test "$line"&&reversed_num=$(echo "$line" | rev)&&a=$lcm&&b=$reversed_num
+while test $b -ne 0
+do temp=$b&&b=`expr $a % $b`
+a=$temp
+done&&lcm=$(echo "$lcm*$reversed_num/$a"|bc)
+done<"$1"&&echo "LCM:$lcm"
