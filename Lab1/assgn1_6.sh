fe16f6a24eeee78939fd7d11d3b5c231e0320f4f
Sarita-Singh <75107393+Sarita-Singh@users.noreply.github.com>
2023-01-25 02:06:06 +0530

    corrected solution ques 6

--- Lab1/assgn1_6.sh
+++ Lab1/assgn1_6.sh
@@ -1,8 +1,24 @@
-sieve="$(seq 2 1000|sort)"
-for n in 2 $(seq 3 2 1000)
-do
-  sieve="$(comm -23 <(echo "$sieve") <(seq $(($n * $n)) $n 1000|sort))"
+declare -a smallest_prime vis
+for ((i=2; i<=1000000; i+=2)); do smallest_prime[i]=2; done
+for ((i=3; i<=1000000; i+=2)); do
+  if ! [[ ${vis[$i]} ]]; then
+        smallest_prime[$i]=$i;
+        for ((j=$i; $j*$i<=1000000; j+=2));do
+            if ! [[ ${vis[$j*$i]} ]]; then
+                  vis[$j*$i]=1 
+                  smallest_prime[$j*$i]=$i;
+                fi
+        done
+	fi
 done
-while read line; do
-  echo "$sieve" | sort -n | awk -v inputlimit="$line" '$1 <= inputlimit { if(inputlimit%$1==0) print }'   | sed ':a;N;$!ba;s/\n/\ /g'
+while read line;do
+    declare -a prime_divisors=()
+    num=$(echo "$line" | tr -d '\r')
+    while [[ $num -ne 1 ]] 
+    do
+            prime_divisors+=(${smallest_prime[$num]})
+            ans=`expr $num / ${smallest_prime[$num]}`
+            num=$ans
+    done
+    echo "${prime_divisors[@]}"
 done < "$1" > "output.txt"
