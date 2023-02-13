1f28cbd2ee3804f361f655001bb7ab844b6e3d42
chirag-ghosh <cghosh828049@gmail.com>
2023-02-13 15:08:38 +0530

    Add macros header for all files;

---
+++ Lab3/macros.h
@@ -0,0 +1,12 @@
+#ifndef _MACROS_H
+#define _MACROS_H
+
+#define FILE_PATH "facebook_combined.txt"
+#define MAX_INT 5000
+#define KEY 1234
+#define FILE_PATH_FOR_KEY "main.c"
+#define PROJECT_ID 1
+#define ROWS 8000 // double the number of initial rows
+#define COLS 8000 // 4 times the number of initial rows
+
+#endif
