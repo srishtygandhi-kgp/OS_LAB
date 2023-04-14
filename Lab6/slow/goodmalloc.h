ab762b7c86bd3a7ec869ff2bae51e120fe9bc5be
Yatindra Indoria <84220034+yatindra7@users.noreply.github.com>
2023-04-14 22:37:16 +0530

    added the footprint;

--- Lab6/slow/goodmalloc.h
+++ Lab6/slow/goodmalloc.h
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 enum ERR {
     ENOMEM = -15
@@ -8,8 +9,10 @@ enum ERR {
     , ENOLST
 };
 
+#define LOGFILE "memfootprint.csv"
 #define SUCCESS 0
 #define RANDLIM 100001
+#define LOGCTL 1
 
 typedef struct node {
     struct node * lnode;
