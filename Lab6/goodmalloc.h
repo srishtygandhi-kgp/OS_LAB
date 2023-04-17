bd735bb97ae380c47c2dbc9f45e74100eba51599
Yatindra Indoria <84220034+yatindra7@users.noreply.github.com>
2023-04-17 15:40:06 +0530

    restructuring;

--- Lab6/goodmalloc.h
+++ Lab6/goodmalloc.h
@@ -1,14 +1,19 @@
-#define NAME_MAXLEN 200
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 enum ERR {
-    ESYSMEM = -15
-    , EMEMEXIST
-    , EMEMINIT
-    , ENOMEM
-    , ENOLIST
-    , EOUTOFRANGE
-    , ENAMEUSE
+    ENOMEM = -15
+    , EINSUF
+    , ELIBMEM
+    , ENOLST
 };
+
+#define LOGFILE "memfoot_nf.csv"
+#define SUCCESS 0
+#define RANDLIM 100001
+#define LOGCTL 0
+#define NOFREE 0
 
 typedef struct node {
     struct node * lnode;
@@ -17,50 +22,16 @@ typedef struct node {
 } Node;
 
 typedef struct list {
-    char name[NAME_MAXLEN];
     Node * root;
     size_t nelems;
 } List;
 
-/**
-* creates a memory of size msize using malloc call, and inits
-* the memory management structures
-* 
-* @param msize [in] size of the memory to init
-* @return errorcode if error, or 0 on success
-* 
-*/
+void createMem(size_t);
 
-int createMem(size_t msize);
+int createList(List*, size_t);
 
-/**
-* creates a list of nlist elements, element has 2 pointres and
-* one integer value
-* 
-* @param nelems [in] takes the number of elements to be allocate
-* @param lname [in] adds the linked list in the name given
-* @param mem [in] the memory segment to add to
-* @return returns the list
-*/
+void initFunc();
 
-List* createList(size_t nelems, char *lname);
+int freeElem(List *);
 
-/**
-* assigns value to a given element in the list
-* 
-* @param lname [in] list name
-* @param offset [in] offset to assign to
-* @param value [in] the value to be assigned
-* @return error code on error, 0 on success
-*/
-
-int assignVal(char *lname, unsigned int offset, int value);
-
-/**
-* frees a list if given the name, if not, then frees all the lists
-* not in use at the moment (if input is null)
-* 
-* @param lname [in] list name or Null
-*/
-
-int freeElem(char *lname);
+int assignVal(List *, unsigned int, int);
